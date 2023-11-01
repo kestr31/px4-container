df0b0f33edc287e81d5d906f5dc4346e1b0f7e6d
kestrel <kestrel@inha.edu>
2023-11-02 01:30:17 +0900

    Changed map generation trigger to watch release command. Added delay.

--- AirSim-Gazebo/python/genMap.py
+++ AirSim-Gazebo/python/genMap.py
@@ -1,7 +1,9 @@
 import os
+import time
 from pykeyboard import PyKeyboard
 
 k = PyKeyboard()
 os.system('xdotool search --name "ForestDeploy" | xargs xdotool windowactivate')
 k.press_key('G')
+time.sleep(3)
 k.release_key('G')
